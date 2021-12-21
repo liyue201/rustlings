28fc39d40c5e91b54c57b5277b1c21c476ea2cdb
liyue <liyue201@126.com>
2021-12-21 14:58:16 +0800

    error_handling done

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -8,7 +8,6 @@
 
 // Make these tests pass! Execute `rustlings hint errors6` for hints :)
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
@@ -16,24 +15,37 @@ use std::num::ParseIntError;
 #[derive(PartialEq, Debug)]
 enum ParsePosNonzeroError {
     Creation(CreationError),
-    ParseInt(ParseIntError)
+    ParseInt(ParseIntError),
 }
 
 impl ParsePosNonzeroError {
     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
-    // TODO: add another error conversion function here.
+    fn from_parseInt(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str)
-    -> Result<PositiveNonzeroInteger, ParsePosNonzeroError>
+                     -> Result<PositiveNonzeroInteger, ParsePosNonzeroError>
 {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
-    PositiveNonzeroInteger::new(x)
-        .map_err(ParsePosNonzeroError::from_creation)
+
+    let x = s.parse().map_err(ParsePosNonzeroError::from_parseInt)?;
+
+    PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
+
+    // match s.parse() {
+    //     Ok(x) => {
+    //         PositiveNonzeroInteger::new(x)
+    //             .map_err(ParsePosNonzeroError::from_creation)
+    //     }
+    //     Err(err) => {
+    //         Err(ParsePosNonzeroError::from_parseInt(err))
+    //     }
+    // }
 }
 
 // Don't change anything below this line.
