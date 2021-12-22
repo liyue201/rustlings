5d8ab1558f4da99dbeb1c1f6721ced5c423fe585
liyue <liyue201@126.com>
2021-12-22 11:41:49 +0800

    standard_library_types done

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -3,7 +3,6 @@
 // can offer. Follow the steps to complete the exercise.
 // As always, there are hints if you execute `rustlings hint iterators2`!
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -12,7 +11,9 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            first.to_uppercase().chain(c).collect()
+        }
     }
 }
 
@@ -21,7 +22,7 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words.iter().map(|&x| capitalize_first(x)).collect()
 }
 
 // Step 3.
@@ -29,7 +30,16 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    // words.iter().filter_map(|&word|
+    //     match word.trim().len() {
+    //         0 => None,
+    //         _ => Some(capitalize_first(word.trim())),
+    //     }
+    // ).collect::<Vec<String>>().join(" ")
+
+    words.iter().filter(|&word| word.trim().len() > 0
+    ).map(|&word| capitalize_first(word)
+    ).collect::<Vec<String>>().join(" ")
 }
 
 #[cfg(test)]
--- exercises/standard_library_types/iterators3.rs
+++ exercises/standard_library_types/iterators3.rs
@@ -6,15 +6,14 @@
 //    list_of_results functions.
 // Execute `rustlings hint iterators3` to get some hints!
 
-// I AM NOT DONE
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub enum DivisionError {
     NotDivisible(NotDivisibleError),
     DivideByZero,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub struct NotDivisibleError {
     dividend: i32,
     divisor: i32,
@@ -22,20 +21,48 @@ pub struct NotDivisibleError {
 
 // Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
-pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {}
+pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
+    if b == 0 {
+        return Err(DivisionError::DivideByZero);
+    }
+    let d = a / b;
+    if b * d == a {
+        return Ok(d);
+    }
+    Err(DivisionError::NotDivisible(NotDivisibleError { dividend: a, divisor: b }))
+}
 
 // Complete the function and return a value of the correct type so the test passes.
 // Desired output: Ok([1, 11, 1426, 3])
-fn result_with_list() -> () {
+fn result_with_list() -> Result<Vec<i32>, DivisionError> {
     let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    let division_results = numbers.iter().map(|&n| divide(n, 27)).filter(
+        |&x| match x {
+            Ok(_) => true,
+            Err(_) => false
+        }
+    ).map(
+        |x| match x {
+            Ok(v) => v,
+            Err(_) => 0
+        }
+    ).collect::<Vec<i32>>();
+
+    Ok(division_results)
 }
 
 // Complete the function and return a value of the correct type so the test passes.
 // Desired output: [Ok(1), Ok(11), Ok(1426), Ok(3)]
-fn list_of_results() -> () {
+fn list_of_results() -> Vec<Result<i32, DivisionError>> {
     let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    // numbers.iter().map(|&n| divide(n, 27)).filter(
+    //     |&x| match x {
+    //         Ok(_) => true,
+    //         Err(_) => false
+    //     }
+    // ).collect()
+
+    numbers.into_iter().map(|number| divide(number, 27)).collect()
 }
 
 #[cfg(test)]
@@ -53,7 +80,7 @@ mod tests {
             divide(81, 6),
             Err(DivisionError::NotDivisible(NotDivisibleError {
                 dividend: 81,
-                divisor: 6
+                divisor: 6,
             }))
         );
     }
